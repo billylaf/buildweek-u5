374f21f58e70457c0c99c4be861052087006cf07
bilal lafdili <bilallafdili23@gmail.com>
2026-07-20 15:02:31 +0200

    Initial commit

---
+++ src/test/java/team4/buildweek_u5/BuildweekU5ApplicationTests.java
@@ -0,0 +1,13 @@
+package team4.buildweek_u5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildweekU5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
