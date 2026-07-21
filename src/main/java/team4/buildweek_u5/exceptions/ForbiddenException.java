b768ab1bd299115cd989f328c526906ddb83f780
antonio.schettini <antonio.schettini93@gmail.com>
2026-07-21 11:19:04 +0200

    first commit - add lifecycle cliente fattura stato fattura + exceptions
    primo commit ciclo vita completo cliente fattura stato fattura, entities, repository, service, controller + aggiunta custom exceptions ed exceptionHandler

---
+++ src/main/java/team4/buildweek_u5/exceptions/ForbiddenException.java
@@ -0,0 +1,7 @@
+package team4.buildweek_u5.exceptions;
+
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
