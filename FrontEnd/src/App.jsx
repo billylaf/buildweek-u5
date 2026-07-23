7b0c7ff97459dd57c18e17d42cf6f33da2d8969b
antonio.schettini <antonio.schettini93@gmail.com>
2026-07-23 12:02:05 +0200

    fix import in app.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./components/login/Login"
-import Sign from "./components/sign/Sign"
-import ListaClienti from "./components/cliente/ListaClienti"
-import CustomNavbar from "./components/navbar/CustomNavbar"
-import "./Style.css"
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/login/Login";
 import Sign from "./components/sign/Sign";
+import ListaClienti from "./components/cliente/ListaClienti";
 import CustomNavbar from "./components/navbar/CustomNavbar";
-import "./Style.css";
 import Dashboard from "./components/dashboard/Dashboard";
+import "./Style.css";
 
 export default function App() {
   return (
@@ -20,13 +15,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Sign />} />
-        <Route path="/clienti" element={<ListaClienti />} />
         {/* Pagina della Dashboard dopo il Login */}
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/clienti" element={<ListaClienti />} />
 
         {/* Reindirizzamento di sicurezza: se la rotta non esiste, torna a "/" */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
